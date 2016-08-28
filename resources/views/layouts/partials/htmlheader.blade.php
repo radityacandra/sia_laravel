cbc74d050bc277a5f9276e2b75fc48836eadc805
radityacandra <radityacandra@gmail.com>
2016-08-28 10:39:49 +0700

    add select2 library

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -18,6 +18,8 @@
     <!-- iCheck -->
     <link href="{{ asset('/plugins/iCheck/square/blue.css') }}" rel="stylesheet" type="text/css" />
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -6,6 +6,8 @@
 <script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
+<!-- Select2 input select library -->
+<script src="{{ asset('/plugins/select2/select2.min.js') }}"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
       Both of these plugins are recommended to enhance the
