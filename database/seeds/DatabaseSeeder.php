1344b38f64b1c3960213608e90138f2ccb4e6b91
radityacandra <radityacandra@gmail.com>
2016-09-10 07:50:39 +0700

    modify database seeder to seed all needed class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,19 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// $this->call(UsersTableSeeder::class);
+		$this->call(ClassSeeder::class);
+		$this->call(ConsentrationSeeder::class);
+		$this->call(StudentSeeder::class);
+		$this->call(SubjectSeeder::class);
+		$this->call(TeacherSeeder::class);
+		$this->call(StudentMemberClassSeeder::class);
+	}
 }
